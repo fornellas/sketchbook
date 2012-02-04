5719cc003c267554ecb896901a459fdedc9181b5
Fabio Pugliese Ornellas <fabio.ornellas@gmail.com>
2012-02-04 17:13:08 -0200

    First working 4096 colors firmware version. - RGB_Backpack_vFPO.c: changed SPI protocol. - Makefile: Changed -Wall to -Werror.

--- RGB_Backpack_vFPO/RGB_Backpack_vFPO.c
+++ RGB_Backpack_vFPO/RGB_Backpack_vFPO.c
@@ -1,7 +1,5 @@
 /* TODO
-  - Validate if SPI input is working.
-    - Single and more boards
-  - Pre-cache color value at interrupt routine to make sendColorValues() faster to decrease flickering.
+  - Test data packing with one color per byte, to see if flickering decreases.
 */
 
 /*****************************************************************************
@@ -51,20 +49,13 @@
 //  | RRRR GGGG | BBBB RRRR | GGGG BBBB |
 //  |   byte1   |   byte2   |   byte3   |
 // Each color has 4bits, and one pixel needs 12bis or 1,5 byte.
-volatile uint8_t receiveBuffer[DISPLAY_BUFFER_SIZE];
-uint8_t displayBuffer[DISPLAY_BUFFER_SIZE];
+volatile uint8_t buffA[DISPLAY_BUFFER_SIZE];
+volatile uint8_t buffB[DISPLAY_BUFFER_SIZE];
+volatile uint8_t *receiveBuffer=buffA;
+volatile uint8_t *displayBuffer=buffB;
 
-// Byte counter for each SPI Transfer
-volatile uint16_t spiByteCount=0;
-
-// true only after a full frame has been buffered
-volatile uint8_t frameReceivedOK=0;
-
-// Number of boards
-volatile uint8_t boardCount;
-
-// Position of this board at the array
-volatile uint8_t thisBoardPosition;
+// Buffer index counter (0-DISPLAY_BUFFER_SIZE)
+volatile uint16_t bufferIndex=0;
 
 //Used for PWM to generate different color brightnesses
 uint8_t timer_clicks=0;
@@ -96,68 +87,21 @@ void ioinit(void) {
   sbi(PORTC, EN);
 
   //Enable SPI / SPI Interrupts
-  SPCR = (1 << SPE) | (1 << SPIE);
+  SPCR=(1<<SPIE)|(1<<SPE);
 }
 
 //
 // SPI Interrupt handler
 //
 
-ISR(SIG_SPI) {
-  // byte received
-  volatile char value;
-  static uint16_t startOfBuffer;
-  static uint16_t endOfBuffer;
-
-  // Halt Interrupts
-  cli();
-
-  // Read SPI byte
-  value = SPDR;
-
-  // First byte is the position of this board at the board array
-  if(0==spiByteCount) {
-    thisBoardPosition=value;
-    startOfBuffer=(2+DISPLAY_BUFFER_SIZE*(thisBoardPosition-1));
-    endOfBuffer=startOfBuffer+DISPLAY_BUFFER_SIZE;
-    // Decrease the board before passing it to the next board
-    SPDR=value-1;
-  } else {
-    // Second byte is the number of boards
-    if(1==spiByteCount)
-      boardCount=value;
-
-    // TODO make better valitation of values
-    if(boardCount>8)
-      boardCount=1;
-    if(thisBoardPosition>boardCount)
-      boardCount=1;
-    
-    // Byte 3+ is image data
-
-    // Pass boardCount or image data to the next board 
-    SPDR=value;
-
-    if(spiByteCount>2) {
-      // If the byte is for this board, store it at the receive buffer
-      if(spiByteCount>=startOfBuffer && spiByteCount<endOfBuffer)
-       receiveBuffer[spiByteCount-startOfBuffer]=value;
-
-      // If we finished receiving, set OK to process buffer
-      if(endOfBuffer-1==spiByteCount)
-        frameReceivedOK=1;
-
-      // If we get more data than we should, invalidate it
-      if(endOfBuffer==spiByteCount)
-        frameReceivedOK=0;
-    }
-  }
-
-  // increment byte counter
-  spiByteCount++;
-
-  // Enable interrupts
-  sei();
+ISR(SPI_STC_vect) {
+  // Store received byte
+  receiveBuffer[bufferIndex++]=SPDR;
+  // Zero counter if needed
+  if(bufferIndex==DISPLAY_BUFFER_SIZE)
+    bufferIndex=0;
+  // Send oldest buffer byte to the next board
+  SPDR=receiveBuffer[bufferIndex];
 }
 
 //
@@ -180,7 +124,8 @@ void sendColorValues(char row_num, uint8_t bitOffset){
     }
     //Lower the shift register clock so we can configure the data
     cbi(PORTC, CLK);
-    //Compare the current color value to timer_clicks to Pulse Width Modulate the led to create the designated brightness
+    //Compare the current color value to timer_clicks to Pulse Width Modulate
+    // the led to create the designated brightness
     if (timer_clicks < value)
       sbi(PORTC, DATA);
     else
@@ -194,9 +139,9 @@ void sendColorValues(char row_num, uint8_t bitOffset){
 // Show display buffer
 //
 
-void showDisplayBuffer(){
+void showDisplayBuffer(void){
   //Increment clicks to determine LED brightness levels, circular 0-15
-  timer_clicks = ++timer_clicks&0x0F;
+  timer_clicks = (++timer_clicks)&0x0F;
   
   //Send all 8 rows of colors to the Matrix
   for(char row_num=0;row_num<8;row_num++){
@@ -214,7 +159,8 @@ void showDisplayBuffer(){
     sbi(PORTC, EN);
     //Put the new data onto the outputs of the shift register
     sbi(PORTC, LATCH);
-    //Sink current through row (Turns colors 'ON' for the given row. Keep in mind that we can only display to 1 row at a time.)
+    //Sink current through row (Turns colors 'ON' for the given row. Keep in
+    // mind that we can only display to 1 row at a time.)
     PORTD=(1<<row_num);
     //Enable the Shift Register Outputs
     cbi(PORTC, EN);
@@ -222,12 +168,12 @@ void showDisplayBuffer(){
 }
 
 //
-// Paint a single color from a pixel
+// Paint a single color of a pixel
 //
 
 void paintColor(uint8_t x, uint8_t y, uint16_t colorOffset, uint8_t value){
   uint16_t startBit;
-  startBit=colorOffset+(y<<3+x)*(BITS_PER_COLOR*3);
+  startBit=colorOffset+((y<<3)+x)*(BITS_PER_COLOR*3);
   if(startBit%8)
     displayBuffer[startBit>>3]=(displayBuffer[startBit>>3]&0xF0)|value;
   else
@@ -247,8 +193,13 @@ void paint(uint8_t x, uint8_t y, uint8_t r, uint8_t g, uint8_t b){
 /*****************************************************************************
  * Main
  *****************************************************************************/
+        
+void main(void){
+  // Slave select variables
+  uint8_t curCS;
+  uint8_t oldCS=1;
 
-int main(void){
+  // Start I/O and Interrupts
   ioinit();
 
   // Show start up pattern
@@ -259,33 +210,35 @@ int main(void){
       paint(x,y+4,0,0,v);
       paint(x,y+6,v,v,v);
     }
-  for(uint16_t c=0;c<5000;c++)
+  for(uint16_t c=0;c<3000;c++)
       showDisplayBuffer();  
 
-  // Clear display buffer
+  // Clear buffers
   for(uint8_t p=0;p<DISPLAY_BUFFER_SIZE;p++)
     displayBuffer[p]=0x00;
+  for(uint8_t p=0;p<DISPLAY_BUFFER_SIZE;p++)
+    receiveBuffer[p]=0x00;
 
   //Enable Global Interrupts
   sei();
 
   //Main loop
   while(1) {
-    //If CS goes high, SPI transfer is complete
-    if (PINB & (1 << CS)) {
-      // Transfer successfull
-      if(frameReceivedOK) {
-        // Copy receive buffer to display buffer ()
-        cli();
-        memcpy((void *)displayBuffer, (void *)receiveBuffer, (size_t)DISPLAY_BUFFER_SIZE);
-        sei();
-      }
-      // Reset counters
-      spiByteCount=0;
-      frameReceivedOK=0;
+    curCS=PINB & (1 << CS);
+    // If CS goes from low to high, SPI transfer is complete
+    if (curCS&&!oldCS) {
+      cli();
+      // swap buffers
+      volatile uint8_t *t;
+      t=displayBuffer;
+      displayBuffer=receiveBuffer;
+      receiveBuffer=t;
+      // Reset counter
+      bufferIndex=0;
+      sei();
     }
+    oldCS=curCS;
     // Display current buffer
     showDisplayBuffer();
   };
-  return 0;
 }
