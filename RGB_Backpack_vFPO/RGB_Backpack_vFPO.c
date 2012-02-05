c679b52a1c70162d6c75690147f2ed251405ab39
Fabio Pugliese Ornellas <fabio.ornellas@gmail.com>
2012-02-05 19:15:46 -0200

    Eliminated flickering.
    Interrupt handler now pre cache RGB values at separate buffers for faster data access at main loop.

--- RGB_Backpack_vFPO/RGB_Backpack_vFPO.c
+++ RGB_Backpack_vFPO/RGB_Backpack_vFPO.c
@@ -44,21 +44,34 @@
  * Global variables
  *****************************************************************************/
 
-// receive / display buffers
 // Data packing:
 //  | RRRR GGGG | BBBB RRRR | GGGG BBBB |
 //  |   byte1   |   byte2   |   byte3   |
-// Each color has 4bits, and one pixel needs 12bis or 1,5 byte.
-volatile uint8_t buffA[DISPLAY_BUFFER_SIZE];
-volatile uint8_t buffB[DISPLAY_BUFFER_SIZE];
-volatile uint8_t *receiveBuffer=buffA;
-volatile uint8_t *displayBuffer=buffB;
 
-// Buffer index counter (0-DISPLAY_BUFFER_SIZE)
-volatile uint16_t bufferIndex=0;
+// Swappable buffers
+volatile uint8_t redBufferA[DISPLAY_PIXELS];
+volatile uint8_t redBufferB[DISPLAY_PIXELS];
+volatile uint8_t greenBufferA[DISPLAY_PIXELS];
+volatile uint8_t greenBufferB[DISPLAY_PIXELS];
+volatile uint8_t blueBufferA[DISPLAY_PIXELS];
+volatile uint8_t blueBufferB[DISPLAY_PIXELS];
+
+// Receive buffers
+volatile uint8_t *receiveBufferRed=redBufferA;
+volatile uint8_t *receiveBufferGreen=greenBufferA;
+volatile uint8_t *receiveBufferBlue=blueBufferA;
+volatile uint8_t receiveBufferRaw[DISPLAY_BUFFER_SIZE];
+volatile uint8_t colorBufferIndex=0;
+volatile uint8_t receiveBufferRawIndex=0;
+volatile uint8_t twoPixels=0;
+
+// Display buffers
+volatile uint8_t *displayBufferRed=redBufferB;
+volatile uint8_t *displayBufferGreen=greenBufferB;
+volatile uint8_t *displayBufferBlue=blueBufferB;
 
 //Used for PWM to generate different color brightnesses
-uint8_t timer_clicks=0;
+uint8_t timerClicks=0;
 
 /*****************************************************************************
  * Functions
@@ -95,38 +108,47 @@ void ioinit(void) {
 //
 
 ISR(SPI_STC_vect) {
-  // Store received byte
-  receiveBuffer[bufferIndex++]=SPDR;
-  // Zero counter if needed
-  if(bufferIndex==DISPLAY_BUFFER_SIZE)
-    bufferIndex=0;
-  // Send oldest buffer byte to the next board
-  SPDR=receiveBuffer[bufferIndex];
+  // Save raw input data
+  receiveBufferRaw[receiveBufferRawIndex]=SPDR;
+
+  // Process data every 3 bytes (2 pixels)
+  if(twoPixels==2){
+    receiveBufferRed[colorBufferIndex]=(receiveBufferRaw[receiveBufferRawIndex-2]&0xF0)>>4;
+    receiveBufferGreen[colorBufferIndex]=receiveBufferRaw[receiveBufferRawIndex-2]&0x0F;
+    receiveBufferBlue[colorBufferIndex]=(receiveBufferRaw[receiveBufferRawIndex-1]&0xF0)>>4;
+    colorBufferIndex++;
+    receiveBufferRed[colorBufferIndex]=receiveBufferRaw[receiveBufferRawIndex-1]&0x0F;
+    receiveBufferGreen[colorBufferIndex]=(receiveBufferRaw[receiveBufferRawIndex]&0xF0)>>4;
+    receiveBufferBlue[colorBufferIndex]=receiveBufferRaw[receiveBufferRawIndex]&0x0F;
+    colorBufferIndex++;
+  }
+
+  // Increment indexes
+  if(colorBufferIndex==DISPLAY_PIXELS)
+    colorBufferIndex=0;
+  receiveBufferRawIndex++;
+  if(receiveBufferRawIndex==DISPLAY_BUFFER_SIZE)
+    receiveBufferRawIndex=0;
+  twoPixels++;
+  if(twoPixels==3)
+    twoPixels=0;
+
+  // Send old data to the next board
+  SPDR=receiveBufferRaw[receiveBufferRawIndex];
 }
 
 //
 // Send Color Values
 //
 
-void sendColorValues(char row_num, uint8_t bitOffset){
-  uint8_t ledMax=(row_num<<3)+8;
-  for (uint8_t led=row_num<<3;led<ledMax;led++){
-    // Get current color
-    uint16_t startBit;
-    uint8_t value;
-    // Find the byte where the pixel color is stored
-    startBit=bitOffset+12*led;
-    if(startBit%8)
-      value=displayBuffer[startBit>>3]&0x0F;
-    else {
-      value=displayBuffer[startBit>>3]&0xF0;
-      value=value>>4;
-    }
+void sendColorValues(char row, volatile uint8_t *colorValue){
+  uint8_t ledMax=(row<<3)+8;
+  for (uint8_t led=row<<3;led<ledMax;led++){
     //Lower the shift register clock so we can configure the data
     cbi(PORTC, CLK);
-    //Compare the current color value to timer_clicks to Pulse Width Modulate
+    //Compare the current color value to timerClicks to Pulse Width Modulate
     // the led to create the designated brightness
-    if (timer_clicks < value)
+    if (timerClicks < colorValue[led])
       sbi(PORTC, DATA);
     else
       cbi(PORTC, DATA);
@@ -141,19 +163,19 @@ void sendColorValues(char row_num, uint8_t bitOffset){
 
 void showDisplayBuffer(void){
   //Increment clicks to determine LED brightness levels, circular 0-15
-  timer_clicks = (++timer_clicks)&0x0F;
+  timerClicks = (++timerClicks)&0xF;
   
   //Send all 8 rows of colors to the Matrix
-  for(char row_num=0;row_num<8;row_num++){
+  for(char row=0;row<8;row++){
     //Disable the shift registers
     cbi(PORTC, LATCH);
 
     //Send Red Values
-    sendColorValues(row_num, 0);
-    //Send Green Values
-    sendColorValues(row_num, 8);
+    sendColorValues(row, displayBufferRed);
     //Send Blue Values
-    sendColorValues(row_num, 4);
+    sendColorValues(row, displayBufferBlue);
+    //Send Green Values
+    sendColorValues(row, displayBufferGreen);
 
     //Disable the Shift Register Outputs
     sbi(PORTC, EN);
@@ -161,33 +183,10 @@ void showDisplayBuffer(void){
     sbi(PORTC, LATCH);
     //Sink current through row (Turns colors 'ON' for the given row. Keep in
     // mind that we can only display to 1 row at a time.)
-    PORTD=(1<<row_num);
+    PORTD=(1<<row);
     //Enable the Shift Register Outputs
     cbi(PORTC, EN);
   }
-}
-
-//
-// Paint a single color of a pixel
-//
-
-void paintColor(uint8_t x, uint8_t y, uint16_t colorOffset, uint8_t value){
-  uint16_t startBit;
-  startBit=colorOffset+((y<<3)+x)*(BITS_PER_COLOR*3);
-  if(startBit%8)
-    displayBuffer[startBit>>3]=(displayBuffer[startBit>>3]&0xF0)|value;
-  else
-    displayBuffer[startBit>>3]=(displayBuffer[startBit>>3]&0x0F)|(value<<BITS_PER_COLOR);
-}
-
-//
-// Paint a single pixel
-//
-
-void paint(uint8_t x, uint8_t y, uint8_t r, uint8_t g, uint8_t b){
-  paintColor(x,8-y-1,0,r);
-  paintColor(x,8-y-1,4,g);
-  paintColor(x,8-y-1,8,b);
 }
 
 /*****************************************************************************
@@ -203,23 +202,42 @@ void main(void){
   ioinit();
 
   // Show start up pattern
-  for(int y=0,v=0;y<2;y++)
-    for(int x=0;x<8;x++,v++) {
-      paint(x,y,v,0,0);
-      paint(x,y+2,0,v,0);
-      paint(x,y+4,0,0,v);
-      paint(x,y+6,v,v,v);
-    }
-  for(uint16_t c=0;c<3000;c++)
+  for(int p=0;p<16;p++){
+    displayBufferRed[p]=p;
+    displayBufferGreen[p]=0;
+    displayBufferBlue[p]=0;
+  }
+  for(int p=16;p<32;p++){
+    displayBufferRed[p]=0;
+    displayBufferGreen[p]=p-16;
+    displayBufferBlue[p]=0;
+  }
+  for(int p=32;p<48;p++){
+    displayBufferRed[p]=0;
+    displayBufferGreen[p]=0;
+    displayBufferBlue[p]=p-32;
+  }
+  for(int p=48;p<64;p++){
+    displayBufferRed[p]=p-48;
+    displayBufferGreen[p]=p-48;
+    displayBufferBlue[p]=p-48;
+  }
+  for(uint16_t c=0;c<5000;c++)
       showDisplayBuffer();  
 
   // Clear buffers
-  for(uint8_t p=0;p<DISPLAY_BUFFER_SIZE;p++)
-    displayBuffer[p]=0x00;
-  for(uint8_t p=0;p<DISPLAY_BUFFER_SIZE;p++)
-    receiveBuffer[p]=0x00;
+  for(uint8_t p=0;p<DISPLAY_PIXELS;p++){
+    displayBufferRed[p]=0x00;
+    displayBufferGreen[p]=0x00;
+    displayBufferBlue[p]=0x00;
+  }
+  for(uint8_t p=0;p<DISPLAY_PIXELS;p++){
+    receiveBufferRed[p]=0x00;
+    receiveBufferGreen[p]=0x00;
+    receiveBufferBlue[p]=0x00;
+  }
 
-  //Enable Global Interrupts
+  //Enable Interrupts
   sei();
 
   //Main loop
@@ -230,11 +248,19 @@ void main(void){
       cli();
       // swap buffers
       volatile uint8_t *t;
-      t=displayBuffer;
-      displayBuffer=receiveBuffer;
-      receiveBuffer=t;
-      // Reset counter
-      bufferIndex=0;
+      t=displayBufferRed;
+      displayBufferRed=receiveBufferRed;
+      receiveBufferRed=t;
+      t=displayBufferGreen;
+      displayBufferGreen=receiveBufferGreen;
+      receiveBufferGreen=t;
+      t=displayBufferBlue;
+      displayBufferBlue=receiveBufferBlue;
+      receiveBufferBlue=t;
+      // Reset counters
+      colorBufferIndex=0;
+      receiveBufferRawIndex=0;
+      twoPixels=0;
       sei();
     }
     oldCS=curCS;
